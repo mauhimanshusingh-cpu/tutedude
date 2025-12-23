ef1bdf501457ae543cf49c2131c4255cdb02aae3
mauhimanshusingh-cpu <mau.himanshusingh@gmail.com>
2025-12-23 18:28:11 +0530

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,29 @@
+
+
+number = int(input("Enter the number: "))
+if number % 2 == 0:
+    print(f"{number} is an odd")
+else:
+    print(f"{number} is an even")
+
+number = int(input("Enter the number: "))
+if number % 2 == 0:
+    print(f"{number} is an odd")
+else:
+    print(f"{number} is an even")
+
+total = 0
+for i in range(1, 51):
+    total = total + i
+print("Sum from 1 to 50 is:", total)
+
+
+
+
+
+
+
+
+
+
+
