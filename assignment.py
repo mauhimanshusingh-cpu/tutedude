b715f58413ebc4b6686047fb51169c284ba364c0
mauhimanshusingh-cpu <mau.himanshusingh@gmail.com>
2026-01-03 10:05:28 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,23 @@
+num1 = int(input("enter a first number: "))
+num2 = int(input("enter a second number: "))
+
+print(f"addition:{num1+num2}")
+print(f"subtraction:{num1-num2}")
+print(f"multiplication:{num1*num2}")
+print(f"division:{num1/num2}")
+
+name1 = input("enter a first  name: ")
+name2 = input("enter a second name: ")
+
+
+
+
+
+
+
+
+
+
+
+
+
